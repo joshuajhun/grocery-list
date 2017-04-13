092495fef17ad0979d006bed464a71f34752990f
Jhun de Andres <joshuajhun@gmail.com>
2017-04-13 11:05:38 -0600

    added one more test for students to write

---
+++ test/Controls.test.js
@@ -0,0 +1,3 @@
+// write a test that will allow you to change the state of the input field,
+// check to see that the state starts as an empty string and that the state also changes 
+// write an assertion that checks to see if you can click the the button and a function is fired.
