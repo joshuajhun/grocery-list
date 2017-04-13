4fd715ca5479f761d196bfd17bafd8a9266e4de8
Jhun de Andres <joshuajhun@gmail.com>
2017-04-13 10:54:35 -0600

    added jest and enzyme to the repo. created a skeleton test for students to write and pass

---
+++ test/RenderList.test.js
@@ -0,0 +1,13 @@
+import RenderList from '../lib/components/RenderList'
+import React from 'react'
+
+describe('RenderList: ', () => {
+  it(' should do the things (change the name of this tests...)', () => {
+
+    // write a test that will check to see if you pass in an array of item objects to the RenderList component
+    // that our state has changed from a single array to an array of 2 objects
+    // check to see the DOM elements ids are actually the correct ids
+    // check to see if the input is equal to what is being displayed on the dom.
+  })
+
+})
